843928d1cd6dc8c580de6e0ab0681987aee66d65
miyagawatakashi <miyagawa@hanjou.co.jp>
2023-11-28 15:45:17 +0900

    ページネーション修正

--- app/components/pagination.js
+++ app/components/pagination.js
@@ -2,13 +2,13 @@ import Link from 'next/link'
 
 const Pagination = (props) => {
     return (
-        <h2  className="paginationWrapper">
+        <div className="p-2 w-full pt-8 mt-8 border-gray-200 text-center">
             {Array.from({ length: props.numberPages }, (_, i) => (
-                <Link key={i + 1} href={ i === 0 ? `/` : `/blog/page/${i + 1}`}>
+                <Link className="ml-5" key={i + 1} href={ i === 0 ? `/` : `/blog/page/${i + 1}`}>
                 {i + 1}
                 </Link>
             ))}
-        </h2>
+        </div>
     )
 }
 
