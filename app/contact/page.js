6ed5b13303cce9838f020766c542807de434349f
miyagawatakashi <miyagawa@hanjou.co.jp>
2023-11-28 11:29:49 +0900

    contactForm実装

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,12 +2,8 @@ const Contact = () => {
     return (
         <>
 		<form action="https://formspree.io/f/xknllbak" method="POST">
-        	{/* <section className="text-gray-600 body-font relative">
-            	<div className="absolute inset-0 bg-gray-300">
-            		<iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe>
-            	</div> */}
             <div className="container px-5 py-24 mx-auto flex">
-            	<div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+            	<div className="lg:w-1/2 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
                 	<h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
                 	<p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
                 	<div className="relative mb-4">
@@ -22,7 +18,6 @@ const Contact = () => {
                 	<p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
               	</div>
             </div>
-			{/* </section> */}
 		</form>
         </>
     )
